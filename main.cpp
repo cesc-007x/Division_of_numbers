87146d0cf1d359355ecde8f0fba4f1b731123ef4
Casper_whiteghost <xchappir@gmail.com>
2026-02-06 21:46:04 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int dividendo, divisore, risultato;
+
+    printf("Inserisci il dividendo: ");
+    scanf("%d", &dividendo);
+
+    printf("Inserisci il divisore: ");
+    scanf("%d", &divisore);
+
+    risultato = dividendo / divisore;
+
+    printf("Il risultato della divisione intera è: %d\n", risultato);
+
+    return 0;
+}
